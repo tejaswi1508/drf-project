cc9ec16d88e8271bd8b3d448d516f7e748bdbd1c
tejaswi1508 <inboxofteju@gmail.com>
2025-03-15 08:34:19 +0530

    serialization

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,26 @@
+import json
+from django.forms.models import model_to_dict
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+# from django.http import JsonResponse
+from products.models import Product
+from products.serializers import ProductSerializer
 
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is the django API homepage response"})
+    instance = Product.objects.all().order_by("?").first()
+    data = {}
+    if instance:
+        # data = model_to_dict(instance, fields=['id','title','content','price'])
+        data = ProductSerializer(instance).data
+        
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
+    # return JsonResponse({"message": "Hi there, this is the django API homepage response"})
+    return Response(data)
 
 # Create your views here.
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,8 +2,8 @@ import requests
 
 endpoint = "http://localhost:8000/api/" # http://127.0.0.1:8000/
 
-get_response = requests.get(endpoint, json={"query": "Hello World"})
-print(get_response.text)
-print(get_response.status_code)
-print(get_response.json()['message'])
-
+get_response = requests.get(endpoint, json={"product_id": 123})
+# print(get_response.text)
+# print(get_response.status_code)
+# print(get_response.json()['message'])
+print(get_response.json())
